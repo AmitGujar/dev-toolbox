af183051b22597bbcb35a250c27fc24fcf4c54be
AmitGujar <amithero3342@gmail.com>
2023-10-12 13:14:51 +0530

    added tf script

--- bash/install.sh
+++ bash/install.sh
@@ -50,7 +50,16 @@ k8s_setup() {
     curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 }
 
+
+terraform_install() {
+    wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    sudo apt update && sudo apt install terraform
+}
+
+
 getting_update
 ansible_setup
 azure_cli
 k8s_setup
+terraform_install
